6419ebda8605493b69a4a0f14800f0cb7c16491e
Your Name <j.e.palmer-1@sms.ed.ac.uk>
2021-01-30 13:11:40 +0000

    stash

--- Code/app.py
+++ Code/app.py
@@ -1866,8 +1866,8 @@ def update_aggregate_figure_ycol_error(fig_yaxis_column,
     return fig
 
 
-if __name__ == '__main__':
-    app.run_server(debug=False, host='192.168.0.36', port=8051)
-
 # if __name__ == '__main__':
-#     app.run_server(debug=False, port=8051)
+#     app.run_server(debug=False, host='192.168.0.36', port=8051)
+
+if __name__ == '__main__':
+    app.run_server(debug=False, port=8051)
